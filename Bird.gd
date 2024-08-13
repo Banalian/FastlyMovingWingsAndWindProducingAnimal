6d5d81c7e348c4daea96351ceba758a15cba5111
Banalian <64379456+Banalian@users.noreply.github.com>
2024-08-13 19:35:30 -0400

    feat(bird): restructure to have a bird scene and allow it to jump

--- Bird.gd
+++ Bird.gd
@@ -1,7 +1,7 @@
-extends Area2D
+extends RigidBody2D
 
 var gravity_force = Vector2.DOWN * 100
-var jump_force = Vector2.UP * 50
+var jump_force = Vector2.UP * 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,11 +11,14 @@ func _ready():
 func _process(delta):
 	position += gravity_force * delta
 
+func jump():
+	set_axis_velocity(jump_force)
+
 func _unhandled_input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_SPACE:
-			position += jump_force
+			jump()
 
 
 func _on_button_pressed():
-	position += jump_force
+	jump()
