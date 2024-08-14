bff91b3b151c9fccb3941115397880876981edc6
Banalian <64379456+Banalian@users.noreply.github.com>
2024-08-14 19:36:17 -0400

    feat(pipe spawner): complete spawner logic and add to main scene

--- pipe_spawner.gd
+++ pipe_spawner.gd
@@ -1,11 +1,13 @@
-extends Node2D
+extends Node
 
 @export var pipe_blueprint: PackedScene
 
 @export var spawnpoint: Node2D
 
+@export var height_range: int = 300
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawn_pipe()
 	pass # Replace with function body.
 
 
@@ -15,7 +17,14 @@ func _process(delta):
 
 
 func spawn_pipe():
+	var new_pipe = pipe_blueprint.instantiate() as Node2D
+	var position = Vector2(spawnpoint.position)
+	position.y += randf_range(position.y + height_range, position.y - height_range)
+	new_pipe.position = position
+	add_child(new_pipe)
+	print("Spawned pipe")
 	pass
 
 func _on_timer_timeout():
+	spawn_pipe()
 	pass # Replace with function body.
